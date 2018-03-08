69a50b1e129a0e37cec87aa09f0c42ab1c2e3e1c
Igor Morais <igorcandeia@gmail.com>
2018-03-07 23:37:57 -0300

    change constraint layout to tests

--- KotlinFirst/app/src/main/java/com/example/igorc/kotlinfirst/MainActivity.kt
+++ KotlinFirst/app/src/main/java/com/example/igorc/kotlinfirst/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.igorc.kotlinfirst
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun buttonClick(view: View) {
+        println(text1.text)
     }
 }
